069006f79eb415451b08a0debb0a7e51dc68ded8
yucinecom9-prog <yucinecom9@gmail.com>
2026-03-26 07:51:18 -0300

    Add files via upload

---
+++                   sw.js
@@ -0,0 +1,10 @@
+const cacheName = 'agropro-v1';
+const assets = ['./', './index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
